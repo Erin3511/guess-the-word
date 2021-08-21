68d4fdabf7b095c7dce1f1374ca42f253183ae5c
Erin3511 <contact@erinbrunet.com>
2021-08-21 11:30:58 -0400

    Accepted and validated player's guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const textInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -8,6 +8,7 @@ const messages = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const symbol = function (word) {
     const placeholder = [];
@@ -22,7 +23,37 @@ symbol(word);
 
 guessButton.addEventListener("click", function (e){
     e.preventDefault();
+    messages.innerText = "";
     const guess = textInput.value;
-    console.log(guess);
+
+    const goodGuess = validateTextInput(guess);
+
+    if (goodGuess){
+        makeGuess(guess);
+    }
     textInput.value = "";
 });
+
+const validateTextInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        messages.innerText = "Please enter a letter from A to Z.";
+    } else if (input.length > 1) {
+        messages.innerText = "Please enter one letter only.";
+    } else if (!input.match(acceptedLetter)) {
+        messages.innerText = "Please enter only one letter from A to Z."
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        messages.innerText = "You've already guessed that letter.  Try again.";
+    } else {
+
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+    }
+};
